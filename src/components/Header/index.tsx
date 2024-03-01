a7f7dd13e41aa8d4ff45d446ddcedf68b2c62ef3
Abrar Anjum <abraranjum10@gmail.com>
2024-03-01 21:31:54 +0500

    changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from 'next/image';
-import Navbar from '@/components/Navbar/index';
+import Navbar from '@/components/Navbar';
 import Logo from '../../../public/assets/images/logo.png';
 
 export default function Header() {
